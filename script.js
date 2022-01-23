ea40730f91ab54d2e9ecd9b9542d7b0562588aa3
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-01-23 15:46:41 +1100

    Score holding functionality
    Added function which holds the score when clicking hold score
    
    Also added logic to change css if player reaches 100 points

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 'use strict';
 
 // Caching elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0; // If activePlayer is 0, then switch to 1 
+        currentScore = 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
+        //classList.toggle will add a class if it is not there, and remove it if it is there - so during the switch, it effects both player elements
+}
 
 // Starting conditions
 score0El.textContent = 0;
@@ -27,7 +40,7 @@ btnRoll.addEventListener('click', function() {
 
     // 2. Displaying dice roll
     diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+    diceEl.src = `dice-${dice}.png`; // Displays the specific image of the dice number rolled with template literals
 
     // 3. Check if dice is 1: if true; switch to next player
     if (dice !== 1) {
@@ -38,9 +51,25 @@ btnRoll.addEventListener('click', function() {
         ).textContent = currentScore;
     } else {
         // Lose all score and switch to next player
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0; // If activePlayer is 0, then switch to 1 
-        currentScore = 0;
+        switchPlayer();
+    }
+})
+
+// Holding score functionality
+btnHold.addEventListener('click', function () {
+    // 1. Adding currentScore to active player's score
+    score[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = score[activePlayer]; // dynamically updates the score using template literals
+    // 2. Is the score over 100? 
+    if (score[activePlayer] >= 20) {
+        // if true: Finish Game
+        document.querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+        // else: Switch player
+        switchPlayer();
     }
 })
 
